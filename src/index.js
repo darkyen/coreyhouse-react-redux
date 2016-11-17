25faf158e1d28028a6c7ffe4174a36a0acd76b46
Cory House <housecor@gmail.com>
2016-11-17 08:20:56 -0600

    Use named import
    I just cloned this repo and confirmed this works.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'babel-polyfill'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 import routes from './routes'
 import './styles/styles.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
+render(
   <Router history={browserHistory} routes={routes}/>,
   document.getElementById('app')
 )
