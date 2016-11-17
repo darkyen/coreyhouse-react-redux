2d890751bcec3131bce3e1679f11c9a68443600f
retorquere <emiliano.heyns@iris-advies.com>
2016-11-17 14:30:41 +0100

    Connecting React to Redux

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Router, browserHistory} from 'react-router'
+import routes from './routes'
+import './styles/styles.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes}/>,
+  document.getElementById('app')
+)
